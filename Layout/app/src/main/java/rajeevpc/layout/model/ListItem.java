f5ec280409e9e3352a05b24f43a22cf13d36132d
rajeeviiit <singhrajiv774@gmail.com>
2016-10-03 19:18:32 +0530

    model,view and controller created

---
+++ Layout/app/src/main/java/rajeevpc/layout/model/ListItem.java
@@ -0,0 +1,7 @@
+package rajeevpc.layout.model;
+
+/**
+ * Created by RajeevPC on 10/3/2016.
+ */
+public class ListItem {
+}
--- Layout/app/src/main/java/rajeevpc/layout/ui/MainActivity.java
+++ Layout/app/src/main/java/rajeevpc/layout/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package rajeevpc.layout;
+package rajeevpc.layout.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import rajeevpc.layout.R;
 
 public class MainActivity extends AppCompatActivity {
 

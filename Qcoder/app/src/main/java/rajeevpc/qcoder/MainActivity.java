31f8c4731541841558546dd18c0f7b533c9abbbd
rajeeviiit <singhrajiv774@gmail.com>
2016-10-14 16:02:26 +0530

    Main2Activity added

--- Qcoder/app/src/main/java/rajeevpc/qcoder/MainActivity.java
+++ Qcoder/app/src/main/java/rajeevpc/qcoder/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
     Button btn;
 
@@ -14,21 +14,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       btn.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-
-           }
-       });
-
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        Intent i=new Intent(MainActivity.this,SearchableActivity.class);
-        startActivity(i);
-
+        btn =(Button) findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(MainActivity.this,WelcomeActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }

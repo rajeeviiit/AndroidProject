95ecf8112ef9c965406cc3482bc9a5e729e03ccf
rajeeviiit <singhrajiv774@gmail.com>
2016-10-14 19:57:17 +0530

    popup window added

--- Qcoder/app/src/main/java/rajeevpc/qcoder/MainActivity.java
+++ Qcoder/app/src/main/java/rajeevpc/qcoder/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i =new Intent(MainActivity.this,TestPopupActivity.class);
                 startActivity(i);
+
             }
         });
 

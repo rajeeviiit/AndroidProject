4589c81a4f57798abd8903b7c8e657d008978470
rajeeviiit <singhrajiv774@gmail.com>
2016-10-21 00:13:09 +0530

    ProfileActivity implemented

--- firebase/app/src/main/java/rajeevpc/firebase/ProfileActivity.java
+++ firebase/app/src/main/java/rajeevpc/firebase/ProfileActivity.java
@@ -1,13 +1,46 @@
 package rajeevpc.firebase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
+    FirebaseAuth firebaseAuth;
+    TextView textViewUserEmail;
+    private Button buttonLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        firebaseAuth=FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser()==null){
+             finish();
+            startActivity(new Intent(this,LoginActivity.class));
+
+        }
+        FirebaseUser user=firebaseAuth.getCurrentUser();
+
+        textViewUserEmail =(TextView)findViewById(R.id.textViewUserEmail);
+        textViewUserEmail.setText("Welcome "+user.getEmail());
+        buttonLogout=(Button)findViewById(R.id.buttonLogout);
+
+        buttonLogout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==buttonLogout){
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(this,LoginActivity.class));
+        }
+
     }
 }

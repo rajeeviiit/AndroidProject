230aac3d5be817c6c36e80c61ceefb95d8824672
rajeeviiit <singhrajiv774@gmail.com>
2016-10-20 23:43:35 +0530

    ProfileActivity created

---
+++ firebase/app/src/main/java/rajeevpc/firebase/ProfileActivity.java
@@ -0,0 +1,13 @@
+package rajeevpc.firebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}

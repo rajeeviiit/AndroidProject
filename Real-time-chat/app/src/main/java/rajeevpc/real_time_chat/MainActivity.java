5ac0cf77350de039d9dc111465ce97fdf9b70230
rajeeviiit <singhrajiv774@gmail.com>
2016-10-21 14:55:22 +0530

    Real-time-chat app created

---
+++ Real-time-chat/app/src/main/java/rajeevpc/real_time_chat/MainActivity.java
@@ -0,0 +1,13 @@
+package rajeevpc.real_time_chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

4891d42682fbbd7bc6c0d22f806ba20efd6d0b5e
rajeeviiit <singhrajiv774@gmail.com>
2016-10-23 00:49:10 +0530

    reference to button,edittext,and listview added

--- Real-time-chat/app/src/main/java/rajeevpc/real_time_chat/MainActivity.java
+++ Real-time-chat/app/src/main/java/rajeevpc/real_time_chat/MainActivity.java
@@ -2,12 +2,25 @@ package rajeevpc.real_time_chat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private Button add_room;
+    private EditText room_name;
+
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list_of_rooms =new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }

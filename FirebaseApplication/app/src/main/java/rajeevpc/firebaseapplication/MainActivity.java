e9b4c0609a508402e0f58c7b71a13a0a27772b88
rajeeviiit <singhrajiv774@gmail.com>
2017-04-02 00:39:36 +0530

    sending data to firebase

--- FirebaseApplication/app/src/main/java/rajeevpc/firebaseapplication/MainActivity.java
+++ FirebaseApplication/app/src/main/java/rajeevpc/firebaseapplication/MainActivity.java
@@ -2,12 +2,35 @@ package rajeevpc.firebaseapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.client.Firebase;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mSendData;
+    private Firebase mRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+
+        mRef = new Firebase("https://fir-application-e5d89.firebaseio.com/");
+
+        mSendData =(Button)findViewById(R.id.sendData);
+
+        mSendData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Firebase mRefChild = mRef.child("Name");
+                mRefChild.setValue("Rajeev");
+
+
+            }
+        });
+
+
     }
 }

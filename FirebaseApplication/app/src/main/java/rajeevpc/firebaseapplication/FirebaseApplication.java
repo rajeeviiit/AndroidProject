3edd96163842aace7aafbfbe75c0e1e2745c2d5a
rajeeviiit <singhrajiv774@gmail.com>
2017-04-02 14:22:16 +0530

    dataretrieve deleted

--- FirebaseApplication/app/src/main/java/rajeevpc/firebaseapplication/FirebaseApplication.java
+++ FirebaseApplication/app/src/main/java/rajeevpc/firebaseapplication/FirebaseApplication.java
@@ -1,8 +1,19 @@
 package rajeevpc.firebaseapplication;
 
+import android.app.Application;
+
+import com.firebase.client.Firebase;
+
 /**
  * Created by RajeevPC on 4/2/2017.
  */
 
-public class FirebaseApplication {
+public class FirebaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Firebase.setAndroidContext(this);
+    }
 }

c13eb6457c88e22d650476f5de62a68106984690
rajeeviiit <singhrajiv774@gmail.com>
2017-04-02 13:22:25 +0530

    change

--- FirebaseApplication/app/src/main/java/rajeevpc/firebaseapplication/MainActivity.java
+++ FirebaseApplication/app/src/main/java/rajeevpc/firebaseapplication/MainActivity.java
@@ -1,36 +1,49 @@
 package rajeevpc.firebaseapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
-    private Button mSendData;
-    private Firebase mRef;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity{
+
+    private Button mAddButton;
+    private Button mGetButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Firebase.setAndroidContext(this);
-
-        mRef = new Firebase("https://fir-application-e5d89.firebaseio.com/");
-
-        mSendData =(Button)findViewById(R.id.sendData);
-
-        mSendData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Firebase mRefChild = mRef.child("Name");
-                mRefChild.setValue("Rajeev");
+        mAddButton = (Button) findViewById(R.id.addButton);
+        mGetButton = (Button) findViewById(R.id.getButton);
 
 
-            }
-        });
 
 
+    }
+
+
+    public void DataAddToFirebase(View view) {
+        startActivity(new Intent(MainActivity.this,DataSendToFirebase.class));
+    }
+
+
+    public void GetDataFromFirebase(View view) {
+        startActivity(new Intent(MainActivity.this,DataGetFromFirebase.class));
     }
 }

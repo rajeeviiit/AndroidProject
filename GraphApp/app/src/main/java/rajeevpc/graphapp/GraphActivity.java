ccdce4d704535dbeec22137fdd11ff7b26929eab
rajeeviiit <singhrajiv774@gmail.com>
2017-04-05 02:14:45 +0530

    Change

--- GraphApp/app/src/main/java/rajeevpc/graphapp/GraphActivity.java
+++ GraphApp/app/src/main/java/rajeevpc/graphapp/GraphActivity.java
@@ -40,9 +40,7 @@ public class GraphActivity extends AppCompatActivity{
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
 

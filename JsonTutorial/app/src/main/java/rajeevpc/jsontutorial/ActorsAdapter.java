776d7cd0de94733e41067ab95fbd33c36e0418d9
rajeeviiit <singhrajiv774@gmail.com>
2017-04-11 11:35:13 +0530

    JsonTutorial project added

---
+++ JsonTutorial/app/src/main/java/rajeevpc/jsontutorial/ActorsAdapter.java
@@ -0,0 +1,102 @@
+package rajeevpc.jsontutorial;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ActorsAdapter extends ArrayAdapter<Actors> {
+    ArrayList<Actors> actorList;
+    LayoutInflater vi;
+    int Resource;
+    ViewHolder holder;
+
+    public ActorsAdapter(Context context, int resource, ArrayList<Actors> objects) {
+        super(context, resource, objects);
+        vi = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        Resource = resource;
+        actorList = objects;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // convert view = design
+        View v = convertView;
+        if (v == null) {
+            holder = new ViewHolder();
+            v = vi.inflate(Resource, null);
+            holder.imageview = (ImageView) v.findViewById(R.id.ivImage);
+            holder.tvName = (TextView) v.findViewById(R.id.tvName);
+            holder.tvDescription = (TextView) v.findViewById(R.id.tvDescriptionn);
+            holder.tvDOB = (TextView) v.findViewById(R.id.tvDateOfBirth);
+            holder.tvCountry = (TextView) v.findViewById(R.id.tvCountry);
+            holder.tvHeight = (TextView) v.findViewById(R.id.tvHeight);
+            holder.tvSpouse = (TextView) v.findViewById(R.id.tvSpouse);
+            holder.tvChildren = (TextView) v.findViewById(R.id.tvChildren);
+            v.setTag(holder);
+        } else {
+            holder = (ViewHolder) v.getTag();
+        }
+        holder.imageview.setImageResource(R.drawable.images);
+        new DownloadImageTask(holder.imageview).execute(actorList.get(position).getImage());
+        holder.tvName.setText(actorList.get(position).getName());
+        holder.tvDescription.setText(actorList.get(position).getDescription());
+        holder.tvDOB.setText("B'day: " + actorList.get(position).getDob());
+        holder.tvCountry.setText(actorList.get(position).getCountry());
+        holder.tvHeight.setText("Height: " + actorList.get(position).getHeight());
+        holder.tvSpouse.setText("Spouse: " + actorList.get(position).getSpouse());
+        holder.tvChildren.setText("Children: " + actorList.get(position).getChildren());
+        return v;
+
+    }
+
+    static class ViewHolder {
+        public ImageView imageview;
+        public TextView tvName;
+        public TextView tvDescription;
+        public TextView tvDOB;
+        public TextView tvCountry;
+        public TextView tvHeight;
+        public TextView tvSpouse;
+        public TextView tvChildren;
+
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+
+    }
+}
---
+++ JsonTutorial/app/src/main/java/rajeevpc/jsontutorial/MainActivity.java
@@ -0,0 +1,138 @@
+package rajeevpc.jsontutorial;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.net.ParseException;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.util.EntityUtils;
+
+public class MainActivity extends Activity {
+
+    ArrayList<Actors> actorsList;
+
+    ActorsAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        actorsList = new ArrayList<Actors>();
+        new JSONAsyncTask().execute("http://microblogging.wingnity.com/JSONParsingTutorial/jsonActors");
+
+        ListView listview = (ListView)findViewById(R.id.list);
+        adapter = new ActorsAdapter(getApplicationContext(), R.layout.row, actorsList);
+
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+                                    long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getApplicationContext(), actorsList.get(position).getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    class JSONAsyncTask extends AsyncTask<String, Void, Boolean> {
+
+        ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(MainActivity.this);
+            dialog.setMessage("Loading, please wait");
+            dialog.setTitle("Connecting server");
+            dialog.show();
+            dialog.setCancelable(false);
+        }
+
+        @Override
+        protected Boolean doInBackground(String... urls) {
+            try {
+
+                //------------------>>
+                HttpPost post = new HttpPost(urls[0]);
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpResponse response = httpclient.execute(post);
+
+                // StatusLine stat = response.getStatusLine();
+                int status = response.getStatusLine().getStatusCode();
+
+                if (status == 200) {
+                    HttpEntity entity = response.getEntity();
+                    String data = EntityUtils.toString(entity);
+
+
+                    JSONObject jsono = new JSONObject(data);
+                    JSONArray jarray = jsono.getJSONArray("actors");
+
+                    for (int i = 0; i < jarray.length(); i++) {
+                        JSONObject object = jarray.getJSONObject(i);
+
+                        Actors actor = new Actors();
+
+                        actor.setName(object.getString("name"));
+                        actor.setDescription(object.getString("description"));
+                        actor.setDob(object.getString("dob"));
+                        actor.setCountry(object.getString("country"));
+                        actor.setHeight(object.getString("height"));
+                        actor.setSpouse(object.getString("spouse"));
+                        actor.setChildren(object.getString("children"));
+                        actor.setImage(object.getString("image"));
+
+                        actorsList.add(actor);
+                    }
+                    return true;
+                }
+
+                //------------------>>
+
+            } catch (ParseException e1) {
+                e1.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+
+        protected void onPostExecute(Boolean result) {
+            dialog.cancel();
+            adapter.notifyDataSetChanged();
+            if(result == false)
+                Toast.makeText(getApplicationContext(), "Unable to fetch data from server", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+
+
+
+
+
+}
